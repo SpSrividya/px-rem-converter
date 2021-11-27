1b1d2edf17ca17bf6e819c2aea69edb3d49341a4
srividya.sp <srividya.sp@accenture.com>
2021-11-27 22:03:00 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hello
+      </header>
+      <body>
+Heii
+      </body>
+      <footer>
+Haha
+      </footer>
+    </div>
+  );
+}
+
+export default App;
